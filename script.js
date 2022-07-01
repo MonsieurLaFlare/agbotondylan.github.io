9d2d23c528d697a8ac6077b7775470b22b6baab9
MonsieurLaFlare <104639301+MonsieurLaFlare@users.noreply.github.com>
2022-07-01 17:12:12 +0200

    agbotondylan.github.io

---
+++ script.js
@@ -0,0 +1,14 @@
+# agbotondylan.github.io
+PROJET SITE WEB STATIC EVALUATION
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
